8dd08a6845a64aab196b48b190f956904104536b
Sypth <105406568+Sypth@users.noreply.github.com>
2024-08-07 10:34:32 +0800

    :100: Implementing Rate Limiting to avoid spam problem in the future

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -15,6 +15,7 @@ class AttendeeController extends Controller
 
     public function __construct(){
         $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')->only(['store', 'destroy']);
         $this->authorizeResource(Attendee::class, 'attendee');
     }
 
@@ -40,7 +41,7 @@ class AttendeeController extends Controller
     {
         $attendee = $this->loadRelationships(
             $event->attendees()->create([
-                'user_id' => 1
+                'user_id' => $request->user()->id
             ])
         );
 
@@ -71,7 +72,6 @@ class AttendeeController extends Controller
     public function destroy(Event $event, Attendee $attendee)
     {
         $attendee->delete();
-
         return response(status: 204);
     }
 }
